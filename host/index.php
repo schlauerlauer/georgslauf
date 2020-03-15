807bc679637af146aa80ae7b3c66f63b75448823
Janek Lauer <janek@lauer.email>
2020-03-15 20:48:32 +0100

    add footer

--- host/index.php
+++ host/index.php
@@ -22,8 +22,8 @@ if(isset($login_session) && $_SESSION['rolle'] >= 3) :
 		<div data-role="content" id="home" style="max-width:1200px; margin-left:auto; margin-right:auto;">
 		</div>
 	</div>
-	<div data-role="footer" data-theme="b">
-	<img src="/res/logo.png" style="max-height:80px;"/>
+	<div data-role="footer" data-position="fixed" data-tap-toggle="false" style="overflow:hidden; margin: 0px;">
+		<h2><img src="/res/logo.png" style="max-height:30px;"/></h2>
 	</div>
 </body>
 </html>
