06fcd9c1d043068031fbac974830d40f4e39fad5
Janek Lauer <janek@lauer.email>
2020-03-15 22:04:02 +0100

    change title

--- host/home.php
+++ host/home.php
@@ -3,7 +3,7 @@ include_once '../../includes/connect_gl.php';
 require('../session/session.php');
 if(isset($login_session) && $_SESSION['rolle'] >= 3) :
 ?>
-<h1>Hostseite <?php echo $login_session; ?></h1>
+<h1>Georgslauf 2020 <?php echo $login_session; ?></h1>
 <div class="ui-grid-b ui-responsive">
   <div class="ui-block-a"><input data-icon="back" value="Anmeldeseite" onclick="window.location.href='/stamm'" type="button"></div>
   <div class="ui-block-b"><input data-icon="home" value="Startseite" onclick="window.location.href='/'" type="button"></div>
