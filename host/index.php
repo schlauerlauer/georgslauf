b93577eaf96c1136e8ad27793944a3034a2a512c
Janek Lauer <janek@lauer.email>
2020-03-15 20:39:31 +0100

    simpler index

--- host/index.php
+++ host/index.php
@@ -19,22 +19,6 @@ if(isset($login_session) && $_SESSION['rolle'] >= 3) :
 </head>
 <body>
 	<div data-role="page" data-theme="b" id="georgslauf">
-		<div data-role="header" data-position="fixed" data-tap-toggle="false" align="center">
-			<table border="0" width="100%">
-			<tr>
-				<td style="width:5%;" align="center"><img src="../res/logo.png" style="max-height:70px;"/></td>
-				<td style="width:90%;">
-					<div data-role="navbar" id="menu">
-						<ul>
-							<li><a target="home.php" class="link ui-btn-active" style="height:30px; text-shadow:none;">Hostseite</a></li>
-							<li><a target="gruppen.php" class="link" style="height:30px; text-shadow:none;">Gruppen</a></li>
-							<li><a target="posten.php" class="link" style="height:30px; text-shadow:none;">Posten</a></li>
-						</ul>
-					</div>
-				</td>
-			</tr>
-			</table>
-		</div>
 		<div data-role="content" id="home" style="max-width:1200px; margin-left:auto; margin-right:auto;">
 		</div>
 	</div>
