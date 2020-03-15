f9701454c3496ef9bf867b892826ef19c6c80415
Janek Lauer <janek@lauer.email>
2020-03-15 13:50:17 +0100

    remove txt creation

--- host/files/gruppenDurch.php
+++ host/files/gruppenDurch.php
@@ -4,19 +4,6 @@ require('../../session/session.php');
 include_once '../settings.php';
 
 if(isset($login_session) && $_SESSION['rolle'] >= 3) {
-  $file = fopen("../../../siegerehrung/gruppen_durchschnitt.md","w") or die("Einlesen der HTML Datei fehlgeschlagen.");
-  $txt = "---
-type: slide
-slideOptions:
-  transition: slide
----
-
-# Georgslauf 2020
-
-Siegerehrung
-
----
-";
   $position = null;
   $stufenwertung = array(0,0,0,0);
   echo '<ol data-role="listview" data-count-theme="b" data-inset="true">';
@@ -37,19 +24,7 @@ ORDER BY durchschnitt DESC, kurz ASC")) {
       echo '<li><a href="#">'.$position.'. Platz ('.$stufenwertung[$stufe].'. der '.$Stufe[$stufe].') "'.$name.'" - '.$stamm.' ('.$kurz.')<span class="ui-li-count">'.$punkte.'</span></a></li>';
       $prev_punkte = $punkte;
       $prev_stufe = $stufe;
-      $txt .="
-## $position. Platz
-
-### $stufenwertung[$stufe]. Platz der $Stufe[$stufe]
-Mit **".round($punkte,2)."** Punkten im Durchschnitt
-
-### *$name* vom Stamm *$stamm*
-
----
-";
     }
   }
   echo '</ol>';
-  fwrite($file, $txt);
-  fclose($file);
 }
