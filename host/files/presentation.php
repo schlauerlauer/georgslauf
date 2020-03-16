bed1746fb30bc4cff78b0d510dd44b3c8c8eca9c
Janek Lauer <janek@lauer.email>
2020-03-16 10:26:16 +0100

    fix

--- host/files/presentation.php
+++ host/files/presentation.php
@@ -4,7 +4,7 @@ require('../../session/session.php');
 include_once '../settings.php';
 
 if(isset($login_session) && $_SESSION['rolle'] >= 3) {
-  echo "<h2>Erstelle Siegertabelle Präsentation</h2>";
+  //echo "<h2>Erstelle Siegertabelle Präsentation</h2>";
   $file = fopen("../../../siegerehrung/siegerehrung.md","w") or die("Einlesen der MD Datei fehlgeschlagen.");
   $md = "---
 type: slide
