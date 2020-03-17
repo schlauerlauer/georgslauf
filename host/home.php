11fd6e55f35447dbdce49c6f24ca7597c23f3b13
Janek Lauer <janek@lauer.email>
2020-03-17 22:13:08 +0100

    change icons

--- host/home.php
+++ host/home.php
@@ -15,7 +15,7 @@ if(isset($login_session) && $_SESSION['rolle'] >= 3) :
   <div class="ui-block-c host" host="gruppenKurz"><input data-icon="bullets" data-theme="a" value="Gruppenkürzel" type="button"></div>
   <div class="ui-block-a host" host="setPLocation"><input data-icon="location" data-theme="a" value="Posten Positionen" type="button"></div>
   <div class="ui-block-b host" host="startGruppen"><input data-icon="edit" data-theme="a" value="Posten Startgruppen" type="button"></div>
-  <div class="ui-block-c confirm" host="deleteAll"><input style="background:red;" data-icon="edit" data-theme="b" value="Alle Posten und Gruppen löschen" type="button"></div>
+  <div class="ui-block-c confirm" host="deleteAll"><input style="background:red;" data-icon="forbidden" data-theme="b" value="Alle Posten und Gruppen löschen" type="button"></div>
 </div>
 <h3>Punkte</h3>
 <div class="ui-grid-b ui-responsive">
@@ -23,7 +23,7 @@ if(isset($login_session) && $_SESSION['rolle'] >= 3) :
   <div class="ui-block-b host" host="postenPunkte"><input data-icon="arrow-r" data-theme="a" value="Posten Punkte eintragen" type="button"></div>
   <div class="ui-block-c host" host="antiCheat"><input data-icon="eye" data-theme="a" value="Wächterwanze" type="button"></div>
   <div class="ui-block-a host" host="query"><input data-icon="grid" data-theme="b" value="Query" type="button"></div>
-  <div class="ui-block-b confirm" host="fill0"><input data-icon="edit" data-theme="b" style="background:red;" value="Punktetabelle mit Nullen füllen" type="button"></div>
+  <div class="ui-block-b confirm" host="fill0"><input data-icon="forbidden" data-theme="b" style="background:red;" value="Punktetabelle mit Nullen füllen" type="button"></div>
   <div class="ui-block-c confirm" host="deletePoints"><input data-icon="forbidden" style="background:red;" data-theme="b" value="Alle Punkte löschen" type="button"></div>
   <!--<div class="ui-block-a host" host="backupPunkte"><input data-icon="arrow-d" disabled data-theme="b" value="Punkte Backup erstellen" type="button"></div>-->
   <!--<div class="ui-block-c host" host="gruppenGröße"><input data-icon="plus" disabled data-theme="a" value="Gruppengröße" type="button"></div>-->
