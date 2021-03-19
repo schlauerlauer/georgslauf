8442758a34f7f296ad8d8316ae49a7e4123f84ea
simon-the-au <simon_au@gmx.de>
2021-03-19 15:18:48 +0100

    test

--- session/login.php
+++ session/login.php
@@ -1,5 +1,5 @@
 ﻿<?php
-include_once '../../includes/connect_gl.php';
+include_once '/var/www/vhosts/hosting101172.af98b.netcup.net/www/georgslauf/includes/connect_gl.php';
 
 session_start();
 $row = '';
