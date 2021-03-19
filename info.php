2ea4845e41923f59ade0ae5aedec1571d90249a2
simon-the-au <simon_au@gmx.de>
2021-03-19 14:49:57 +0100

    new timetable

--- info.php
+++ info.php
@@ -4,21 +4,43 @@
 <h3>Hallo und herzlich Willkommen, München-Isar!</h3>
 <p>Wir freuen uns mit Euch auf den 41. Georgslauf 2020+1!<br>
 Am <strong>Sonntag, 11.07.2021</strong> begrüßt Euch PRM bei euch zuhause!<br>
-Los geht es <strong>pünktlich um 08 Uhr</strong> bei gutem Indieze bei euch im Pfaarsaal.<br>
+Los geht es <strong>pünktlich um 09 Uhr</strong> bei guter Inzidenz bei euch im Pfaarsaal.<br>
 
 <br>
 Unser Zeitplan für den 11.07.2021 sieht dann folgendermaßen aus:<br>
-<ul>
-  <li>08:00 Uhr: Treffpunkt & Check-In im Pfarrsaal</li>
-  <li>09:00 Uhr: Gottesdienst in St. Ursula</li>
-  <li>09:00 Uhr: Postenbriefing im Pfarrsaal (1 P. p. P.)</li>
-  <li>10:30 Uhr: Startschuss</li>
-  <li>13:00 bis 13:30 Uhr: Mittagspause</li>
-  <li>17:30 Uhr: Ende des Laufs</li>
-  <li>ab 17:30 Uhr: Abendessen</li>
-  <li>19:00 Uhr: Siegerehrung</li>
-  <li>circa 20:30 Uhr: Ende</li> 
-</ul>
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:hidden;border-width:1px;overflow:hidden;word-break:normal;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+.tg .tg-q64d{background-color:#ffffff;color:#333333}
+</style>
+<table class="tg">
+  <tr>
+    <th class="tg-031e">08:00 Uhr<br></th>
+    <th class="tg-031e">Treffen in den Stämmen<br></th>
+  </tr>
+  <tr>
+    <td class="tg-031e">09:00 Uhr<br></td>
+    <td class="tg-031e">Spiritueller Auftackt<br></td>
+  </tr>
+  <tr>
+    <td class="tg-031e">10:30 Uhr<br></td>
+    <td class="tg-031e">Startschuss</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">12:30 Uhr bis 13:30 Uhr<br></td>
+    <td class="tg-031e">Mittagspause</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">16:30 Uhr<br></td>
+    <td class="tg-031e"><br>Postenschluss</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">18:00 Uhr<br></td>
+    <td class="tg-q64d">Siegerehrung</td>
+  </tr>
+</table>
+
 <br>
 Wir freuen uns auf einen tollen Tag und hoffen auf gutes Wetter und gutes Infektionsgeschehen!  <br>
 Euer Stamm PRM<br>
