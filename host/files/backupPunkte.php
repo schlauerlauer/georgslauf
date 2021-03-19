401cf44d5eb5d13e7b40aa4e23a59f9761cd39af
Janek Lauer <janek@lauer.email>
2021-03-19 18:51:18 +0100

    cleanup include path

--- host/files/backupPunkte.php
+++ host/files/backupPunkte.php
@@ -1,5 +1,5 @@
 <?php
-include_once '/var/www/vhosts/hosting101172.af98b.netcup.net/www/georgslauf/master/includes/connect_gl.php';
+include_once $__SERVER["DOCUMENT_ROOT"].'/includes/connect_gl.php';
 include_once '../settings.php';
 require('../../session/session.php');
 
