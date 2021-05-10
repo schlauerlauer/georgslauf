fa5635f713d4001cd95bd6b607195826cdf95f06
Janek Lauer <janek@lauer.email>
2021-05-10 19:04:44 +0200

    add put option

--- controllers/group.go
+++ controllers/group.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"georgslauf/models"
@@ -9,17 +10,37 @@ import (
 type CreateGroupInput struct {
 	Name	string	`json:"name"	binding:"required"`
 	Size	uint	`json:"size"	binding:"required"`
+	TribeID	uint	`json:"tribeid"	binding:"required"`
+	Short 	string	`json:"short"`
+	RoleID	uint	`json:"role"`
+	Details	string	`json:"details"`
+	Contact	string	`json:"contact"`
+
 }
 
 type UpdateGroupInput struct {
 	Name	string	`json:"name"`
 	Size	uint	`json:"size"`
+	Short 	string	`json:"short"`
+	RoleID	uint	`json:"role"`
+	Details	string	`json:"details"`
+	Contact	string	`json:"contact"`
 }
 
 func GetGroups(c *gin.Context) {
 	var groups []models.Group
-	models.DB.Find(&groups)
-	c.JSON(http.StatusOK, gin.H{"data": groups})
+	if err := models.DB.Find(&groups).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.Header("Access-Control-Expose-Headers", "X-Total-Count")
+		//lastname := c.Query("_end")
+		// start := c.DefaultQuery("_start", "0")
+		// end := c.DefaultQuery("_end", "10")
+		// fmt.Println(start)
+		c.Header("X-Total-Count", "10") //FIXME
+		c.JSON(http.StatusOK, groups)
+	}
 }
 
 func GetGroup(c *gin.Context) {
@@ -29,7 +50,7 @@ func GetGroup(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": group})
+	c.JSON(http.StatusOK, group)
 }
 
 func PostGroup(c *gin.Context) {
@@ -40,9 +61,21 @@ func PostGroup(c *gin.Context) {
 		return
 	}
 	// Create group
-	group := models.Group{Name: input.Name, Size: input.Size}
+	group := models.Group{Name: input.Name, Size: input.Size, TribeID: input.TribeID}
 	models.DB.Create(&group)
-	c.JSON(http.StatusOK, gin.H{"data": group})
+	c.JSON(http.StatusOK, group)
+}
+
+func PutGroup(c *gin.Context) {
+	// Validate input
+	var input CreateGroupInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	// Put group
+	models.DB.Create(&input)
+	c.JSON(http.StatusOK, input)
 }
 
 func PatchGroup(c *gin.Context) {
@@ -59,7 +92,7 @@ func PatchGroup(c *gin.Context) {
 		return
 	}
 	models.DB.Model(&group).Updates(input)
-	c.JSON(http.StatusOK, gin.H{"data": group})
+	c.JSON(http.StatusOK, group)
 }
 
 func DeleteGroup(c *gin.Context) {
@@ -70,5 +103,5 @@ func DeleteGroup(c *gin.Context) {
 		return
 	}
 	models.DB.Delete(&group)
-	c.JSON(http.StatusOK, gin.H{"data": true})
+	c.JSON(http.StatusOK, true)
 }
