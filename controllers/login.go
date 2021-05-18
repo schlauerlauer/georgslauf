97e71ba00f15d5cee2a6e8d2e2cf9be135a60d8a
Janek Lauer <janek@lauer.email>
2021-05-18 20:58:49 +0200

    update

--- controllers/login.go
+++ controllers/login.go
@@ -4,23 +4,31 @@ import (
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"georgslauf/models"
+	"strconv"
 )
 
 type CreateLoginInput struct {
 	Name 		string		`json:"name"		binding:"required"`
 	Password	string		`json:"password"	binding:"required"`
-	//RoleID		uint		`json:"role"		binding:"required"`
+	RoleID		uint		`json:"RoleID"		binding:"required"`
 }
 
 type UpdateLoginInput struct {
 	Name		string		`json:"name"`
 	Password	string		`json:"password"`
+	RoleID		uint		`json:"RoleID"`
 }
 
 func GetLogins(c *gin.Context) {
 	var logins []models.Login
-	models.DB.Find(&logins)
-	c.JSON(http.StatusOK, logins)
+	result := models.DB.Find(&logins)
+	if result.Error != nil {
+		c.AbortWithStatus(500)
+	} else {
+		c.Header("Access-Control-Expose-Headers", "X-Total-Count")
+		c.Header("X-Total-Count", strconv.FormatInt(result.RowsAffected, 10))
+		c.JSON(http.StatusOK, logins)
+	}
 }
 
 func GetLogin(c *gin.Context) {
@@ -39,9 +47,13 @@ func PostLogin(c *gin.Context) {
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
-	}
+	} // TODO error checking multiple unique
 	// Create login
-	login := models.Login{Name: input.Name, Password: input.Password}
+	login := models.Login{
+		Name: input.Name,
+		Password: input.Password,
+		RoleID: input.RoleID,
+	}
 	models.DB.Create(&login)
 	c.JSON(http.StatusOK, login)
 }
