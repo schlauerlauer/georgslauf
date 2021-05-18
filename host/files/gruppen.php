dcad40c0fff96126596822c0029cf360bff88f1f
simon-the-au <simon_au@gmx.de>
2021-05-18 20:16:48 +0200

    changes in host

--- host/files/gruppen.php
+++ host/files/gruppen.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once '/var/www/vhosts/hosting101172.af98b.netcup.net/www/georgslauf/dev/includes/connect_gl.php';
-include_once 'settings.php';
+include_once '../settings.php';
 require('../session/session.php');
 
 
