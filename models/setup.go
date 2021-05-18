304d45ad610d760bfcdd2541bb00af80db2fd78c
Janek Lauer <janek@lauer.email>
2021-05-18 19:05:14 +0200

    add prod db

--- models/setup.go
+++ models/setup.go
@@ -1,14 +1,20 @@
 package models
 
 import (
-	"gorm.io/driver/sqlite"
+	//"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	db, err := gorm.Open(sqlite.Open("./georgslauf.db"), &gorm.Config{
+	// db, err := gorm.Open(sqlite.Open("./georgslauf.db"), &gorm.Config{
+	// 	PrepareStmt: true,
+	// })
+
+	dsn := "***REMOVED***?charset=utf8mb4&parseTime=true&loc=Local&tls=skip-verify"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		PrepareStmt: true,
 	})
 
