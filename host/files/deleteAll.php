b12f15eeb6ff6b15383f46ba3b714a34a6c46564
Janek Lauer <janek@lauer.email>
2021-06-01 21:26:20 +0200

    fix delete all func

--- host/files/deleteAll.php
+++ host/files/deleteAll.php
@@ -3,11 +3,13 @@ include_once '/var/www/vhosts/hosting101172.af98b.netcup.net/www/georgslauf/mast
 require('../../session/session.php');
 
 if(isset($login_session) && $_SESSION['rolle'] >= 3) {
-  if ($stmt = $mysqli->prepare("DELETE FROM gruppen, posten")) {
+  if ($stmt = $mysqli->prepare("DELETE FROM gruppen")) {
     $stmt->execute();
-    echo "Alle Gruppen und Posten gelöscht";
-  }
-  else echo "Etwas ist schiefgelaufen";
-
+    echo "Alle Gruppen gelöscht.";
+  } else echo "Fehler beim Gruppen löschen.";
+  if ($stmt = $mysqli->prepare("DELETE FROM posten")) {
+    $stmt->execute();
+    echo "Alle Posten gelöscht.";
+  } else echo "Fehler beim Posten löschen.";
 } else echo "Keine Berechtigung";
 ?>
