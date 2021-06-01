4302fea25bb9d6d61139f5f136e90363780d1d2b
Janek Lauer <janek@lauer.email>
2021-06-01 15:10:18 +0200

    add top view

--- models/setup.go
+++ models/setup.go
@@ -33,6 +33,40 @@ func ConnectDatabase() {
 		&Grouping{},
 		&Content{},
 	)
+	db.Exec("DROP VIEW group_top")
+	db.Exec("DROP VIEW station_top")
+	groupingTopQuery := `
+		CREATE VIEW group_top AS
+		SELECT
+			group_id AS 'id',
+			groups.name AS 'group',
+			groupings.name AS 'grouping',
+			tribes.name AS 'tribe',
+			sum(value) as 'sum'
+		FROM group_points
+		INNER JOIN groups on groups.id = group_id
+		INNER JOIN groupings on groupings.id = groups.grouping_id
+		INNER JOIN tribes on tribes.id = groups.tribe_id
+		GROUP BY groups.name
+		ORDER BY sum DESC
+		;
+	`
+	stationTopQuery := `
+		CREATE VIEW station_top AS
+		SELECT
+			station_id AS 'id',
+			stations.name AS 'station',
+			tribes.name AS 'tribe',
+			sum(value) as 'sum'
+		FROM station_points
+		INNER JOIN stations on stations.id = station_id
+		INNER JOIN tribes on tribes.id = stations.tribe_id
+		GROUP BY station_id
+		ORDER BY sum DESC
+		;
+	`
+	db.Exec(groupingTopQuery)
+	db.Exec(stationTopQuery)
 
 	DB = db
 
