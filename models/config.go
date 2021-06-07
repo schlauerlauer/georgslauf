6c564a9dbdbfa1c4a6d1fadfa318762fb6c549f9
Janek Lauer <janek@lauer.email>
2021-06-07 19:55:35 +0200

    config

---
+++ models/config.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"time"
+)
+
+type Config struct {
+	ID			uint		`json:"id" gorm:"primary_key"`
+	CreatedAt	time.Time
+	UpdatedAt	time.Time
+	Name		string		`json:"name" gorm:"unique"`
+	ValueB		bool		`json:"valueb" gorm:"not null"`
+}
