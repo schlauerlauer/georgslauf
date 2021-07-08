c259dca54288f933c450334e3ae5494ae35c7578
Janek Lauer <janek@lauer.email>
2021-07-08 10:51:45 +0200

    remove order + kurz

--- host/files/gruppenTop.php
+++ host/files/gruppenTop.php
@@ -7,7 +7,7 @@ if(isset($login_session) && $_SESSION['rolle'] >= 3) {
   echo "<h2>Gruppen Sieger</h2>";
   $position = 0;
   $stufenwertung = array(0,0,0,0);
-  echo '<ol data-role="listview" data-count-theme="b" data-inset="true">';
+  echo '<ul data-role="listview" data-count-theme="b" data-inset="true">';
   if ($stmt = $mysqli->prepare("SELECT kurz, name, stufe, stamm, sum(points) summe
 FROM gruppen, punkte
 WHERE gruppen.id = an
@@ -22,12 +22,12 @@ ORDER BY summe DESC, kurz ASC")) {
         if($prev_stufe == $stufe)  $stufenwertung[$stufe]--;
       }
       $stufenwertung[$stufe]++;
-      echo '<li><a href="#">'.$position.'. Platz ('.$stufenwertung[$stufe].'. der '.$Stufe[$stufe].') "'.$name.'" - '.$stamm.' ('.$kurz.')<span class="ui-li-count">'.$punkte.'</span></a></li>';
+      echo '<li><a href="#">'.$position.'. Platz ('.$stufenwertung[$stufe].'. der '.$Stufe[$stufe].') "'.$name.'" - '.$stamm.'<span class="ui-li-count">'.$punkte.'</span></a></li>';
       $prev_punkte = $punkte;
       $prev_stufe = $stufe;
     }
   }
-  echo '</ol>';
+  echo '</ul>';
 }
 else {
     echo 'Keine Berechtigung.';
