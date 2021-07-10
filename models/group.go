4729f12f70a636caa56c44bcc3568b4c598493f2
Janek Lauer <janek@lauer.email>
2021-07-10 18:54:24 +0200

    grouptribe

--- models/group.go
+++ models/group.go
@@ -14,3 +14,17 @@ type Group struct {
 	GroupingID	uint		`json:"GroupingID" binding:"required" gorm:"foreignKey:GroupingID;not null"`
 	TribeID		uint		`json:"TribeID" binding:"required" gorm:"foreignKey:TribeID;not null"`
 }
+
+type GroupTribe struct {
+	ID			uint		`json:"id"`
+	CreatedAt	time.Time	`json:"created_at"`
+	UpdatedAt	time.Time	`json:"updated_at"`
+	Short		string		`json:"short"`
+	Group		string		`json:"group"`
+	Size		uint		`json:"size"`
+	Tribe		string		`json:"tribe"`
+	LoginID		uint		`json:"tribe_login"`
+}
+func (GroupTribe) TableName() string {
+	return "group_tribe"
+}
