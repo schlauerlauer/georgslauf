e6cd2aa2800ff82c777131e62fa9c7dd10f5018e
Anton Dorp <i-net.sonstiges@web.de>
2022-06-13 19:11:35 +0000

    Geändert auf 2022

--- info.php
+++ info.php
@@ -1,25 +1,25 @@
 <br>
 <h1> GEORGSLAUF 2020+1 Isar Kunterbunt</h1>
-<h2>Organisiert von PRM im schönen München am 11. Juli 2021</h2>
+<h2>Organisiert von PRM im schönen München am 16. Juli 2022</h2>
 <h3>Hallo und herzlich Willkommen, München-Isar!</h3>
-<p>Wir freuen uns mit Euch auf den 41. Georgslauf 2020+1!<br>
-Am <strong>Sonntag, 11.07.2021</strong> begrüßt Euch PRM bei euch zuhause!<br>
-Los geht es <strong>pünktlich um 09 Uhr</strong> bei guter Inzidenz bei euch im Pfaarsaal.<br>
+<p>Wir freuen uns mit Euch auf den 42. Georgslauf 2022!<br>
+Am <strong>Sonntag, 16.07.2022</strong> begrüßt Euch PRM im schönen Schwabing!<br>
+Startschuss ist <strong>pünktlich um 09 Uhr</strong> an der Ursl.<br>
 
 <br>
-Unser Zeitplan für den 11.07.2021 sieht dann folgendermaßen aus:<br>
+Unser Zeitplan für den 16.07.2022 sieht dann folgendermaßen aus:<br>
 <ul>
-  <li>08:00 Uhr: Treffen in euren Stämmen</li>
-  <li>09:00 Uhr: Spiritueller Auftakt</li>
-  <li>10:30 Uhr: Startschuss</li>
+  <li>07:00 Uhr: Treffen bei PRM</li>
+  <li>07:45 Uhr: Auftakt</li>
+  <li>09:30 Uhr: Startschuss</li>
   <li>12:30 bis 13:00 Uhr: Mittagspause</li>
   <li>16:30 Uhr: Ende des Laufs</li>
   <li>18:00 Uhr: Sieger*innen Ehrung</li>
   <li>circa 20:30 Uhr: Ende</li> 
-  <li>circa 21:30 Uhr: Party</li> 
+  <li>circa 22:00 Uhr: Party</li> 
 </ul>
 <br>
-Wir freuen uns auf einen tollen Tag und hoffen auf gutes Wetter und gutes Infektionsgeschehen!  <br>
+Wir freuen uns auf einen tollen Tag und hoffen auf gutes Wetter!  <br>
 Euer Stamm PRM<br>
 <h1 id="clock"></h1>
 <br>
