7f1b9da71276bda42e73dae09554b5f2a6852505
Janek Lauer <janek@lauer.email>
2022-06-27 15:06:31 +0200

    fix dev

--- session/login.php
+++ session/login.php
@@ -1,5 +1,5 @@
 ﻿<?php
-include_once '/var/www/vhosts/hosting101172.af98b.netcup.net/www/georgslauf/master/includes/connect_gl.php';
+include_once '/var/www/vhosts/hosting101172.af98b.netcup.net/www/georgslauf/dev/includes/connect_gl.php';
 session_start();
 $row = '';
 if (isset($_POST['un'])) {
--- session/session.php
+++ session/session.php
@@ -1,5 +1,5 @@
 <?php
-include_once '/var/www/vhosts/hosting101172.af98b.netcup.net/www/georgslauf/master/includes/connect_gl.php';
+include_once '/var/www/vhosts/hosting101172.af98b.netcup.net/www/georgslauf/dev/includes/connect_gl.php';
 session_start();
 $user_check=$_SESSION['login_user'];
 $row;
