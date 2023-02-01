f34a526ad014271aceab18adc0dba307f3b2c444
Janek Lauer <janek@lauer.email>
2023-02-01 19:18:53 +0100

    update settings

--- host/settings.php
+++ host/settings.php
@@ -1,7 +1,7 @@
 <?php
 //Einstellungsdatei für den Georgslauf
 
-$Email = "gl22@stamm-prm.de";
+$Email = "anton@sanktansgar.de";
 
 $Kat = array('Wissen & Technik','Spiel & Spaß'); //Die Kategorien für die Posten, beliebig erweiterbar / veränderbar!
 $Stufe = array('Wölflinge','Jupfis','Pfadis','Rover'); // Stufen namen, wie sie angezeigt werden (auf anmeldeseite aber hardcoded!) (in SQL sind nur nummern) eher nicht verändern
@@ -10,17 +10,17 @@ $WKat = array(9,10); //WöPo-Anzahl Posten pro Kategorie - Anzahl an Zahlen muss
 $RKat = array(2,2,); //RoPo-Anzahl Posten pro Kategorie - "
 
 
-$Anmeldung = false; //Stämme können Gruppen und Posten anmelden
-$Abmeldung = false; //Stämme können Gruppen und Posten löschen
-$PAnmeldung = false; //Stämme können Posten anmelden
-$GAnmeldung = false; //Stämme können Gruppen anmelden
+$Anmeldung = true; //Stämme können Gruppen und Posten anmelden
+$Abmeldung = true; //Stämme können Gruppen und Posten löschen
+$PAnmeldung = true; //Stämme können Posten anmelden
+$GAnmeldung = true; //Stämme können Gruppen anmelden
 $ShowLogin = true; //Show Posten Login Information in Stamm -> Posten
 
 $Rollen = array("keine Rechte", "Posten", "Stamm", "Host", "Admin"); //Rechte vergabe
 
-$Posten = array(false, "Die Posten sind noch nicht öffentlich"); // Posten auf Hauptseite sichtbar oder nicht
+$Posten = array(true, "Die Posten sind noch nicht öffentlich"); // Posten auf Hauptseite sichtbar oder nicht
 $PostenStufe = 'Z'; //Trennung RoPo / WöPo großes o, -> siehe Posten -> update.php
-$Punkte = array(false, "Die Posten dürfen noch keine Punkte vergeben!"); //true = Posten dürfen Punkte verteilen, false -> Fehlermeldung;
+$Punkte = array(true, "Die Posten dürfen noch keine Punkte vergeben!"); //true = Posten dürfen Punkte verteilen, false -> Fehlermeldung;
 
 $Ende = false; //Host kann keine Punkte mehr eintragen wenn true;
 $Backup = true; //Erstelle automatische Backups der Datenbank;
