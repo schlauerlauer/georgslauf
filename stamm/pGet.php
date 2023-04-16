8787826cc64fd276d51e0ad7b189d703f0e6dc51
Janek Lauer <janek@lauer.email>
2023-04-16 13:03:15 +0200

    ignore stufe on insert

--- stamm/pGet.php
+++ stamm/pGet.php
@@ -1,19 +1,12 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'].'/includes/connect_gl.php';
 
-$wP;
-$rP;
+$current_posten_pro_kategorie;
 
-if ($stmt = $mysqli->prepare("SELECT sum(case when kategorie = '0' then 1 else 0 end) k0, sum(case when kategorie = '1' then 1 else 0 end) k1, sum(case when kategorie = '2' then 1 else 0 end) k2, sum(case when kategorie = '3' then 1 else 0 end) k3, sum(case when kategorie = '4' then 1 else 0 end) k4 FROM posten WHERE stufe = 'WöPo'")) {
+if ($stmt = $mysqli->prepare("SELECT sum(case when kategorie = '0' then 1 else 0 end) k0, sum(case when kategorie = '1' then 1 else 0 end) k1, sum(case when kategorie = '2' then 1 else 0 end) k2, sum(case when kategorie = '3' then 1 else 0 end) k3, sum(case when kategorie = '4' then 1 else 0 end) k4 FROM posten")) {
 	$stmt->execute();
 	$stmt->store_result();
-	$stmt->bind_result($wP[0], $wP[1], $wP[2], $wP[3], $wP[4]);
-	while ($stmt->fetch()) {}
-}
-if ($stmt = $mysqli->prepare("SELECT sum(case when kategorie = '0' then 1 else 0 end) k0, sum(case when kategorie = '1' then 1 else 0 end) k1, sum(case when kategorie = '2' then 1 else 0 end) k2, sum(case when kategorie = '3' then 1 else 0 end) k3, sum(case when kategorie = '4' then 1 else 0 end) k4 FROM posten WHERE stufe = 'RoPo'")) {
-	$stmt->execute();
-	$stmt->store_result();
-	$stmt->bind_result($rP[0], $rP[1], $rP[2], $rP[3], $rP[4]);
+	$stmt->bind_result($current_posten_pro_kategorie[0], $current_posten_pro_kategorie[1], $current_posten_pro_kategorie[2], $current_posten_pro_kategorie[3], $current_posten_pro_kategorie[4]);
 	while ($stmt->fetch()) {}
 }
 ?>
