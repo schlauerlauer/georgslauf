64b1dbe49c0b4eb52667eaefb1b8230f26a62b7a
Janek Lauer <janek@lauer.email>
2023-04-16 13:18:30 +0200

    fix insert

--- stamm/insert.php
+++ stamm/insert.php
@@ -18,8 +18,6 @@ if (isset ($_POST['gorp'])) {
 			echo 2;
 			return;
 		}
-		echo 3;
-		return;
 		if ($stmt = $mysqli->prepare("INSERT INTO `posten` (`name`,`stamm`,`kategorie`,`beschreibung`,`kontakt`,`anzahl`,`veggie`,`material`,`ort`,`sonstiges`,`stufe`) VALUES (?,?,?,?,?,?,?,?,?,?,?)")) {
 			$stmt->bind_param('sssssssssss', $_POST['pname'], $login_session, $_POST['kid'], $_POST['pdesc'], $_POST['pkont'], $_POST['pval'], $_POST['pveg'], $_POST['pmat'], $_POST['port'], $_POST['psonst'], $_POST['pid']);
 			$stmt->execute();
