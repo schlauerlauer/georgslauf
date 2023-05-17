eb5739aa14f0de6f7e7dd55f7f591f14cb5a41f2
Janek Lauer <janek@lauer.email>
2023-05-18 01:23:44 +0200

    switch to json

--- api/models/config.go
+++ api/models/config.go
@@ -5,9 +5,15 @@ import (
 )
 
 type Config struct {
-    ID          uint        `json:"id" gorm:"primary_key"`
+    ID          uint                    `json:"id" gorm:"primary_key"`
     CreatedAt   time.Time
     UpdatedAt   time.Time
-    Name        string      `json:"name" gorm:"unique"`
-    ValueB      bool        `json:"valueb" gorm:"not null"`
+    Name        string                  `json:"name" gorm:"uniqueIndex"`
+    Value       map[string]interface{}  `gorm:"serializer:json"`
 }
+
+/*
+config names:
+- notice: message
+- contact: slack, tel, whatsapp
+*/
