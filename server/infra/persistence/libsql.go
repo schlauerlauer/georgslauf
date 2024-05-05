dbfa63b1683d97e8b96013880eb1e413b4f46cb9
Janek Lauer <git@lauer.email>
2024-05-05 15:12:16 +0200

    update

--- server/infra/persistence/libsql.go
+++ server/infra/persistence/libsql.go
@@ -3,7 +3,6 @@ package persistence
 import (
 	"database/sql"
 	"embed"
-	"errors"
 	"georgslauf/domain/db"
 	"georgslauf/interfaces/config"
 	"log/slog"
@@ -22,20 +21,36 @@ func NewRepository(config *config.DatabaseConfig, migrations *embed.FS) (*Reposi
 	location, err := time.LoadLocation(config.Timezone)
 	if err != nil {
 		slog.Error("error parsing timezone", "err", err)
+		return nil, err
 	}
 
-	slog.Info("creating connector")
+	var sqlDb *sql.DB
+	if config.EnableReplication {
+		slog.Debug("using replication; creating connector")
+		connector, err := libsql.NewEmbeddedReplicaConnector(config.Path, config.Remote)
+		if err != nil {
+			slog.Error("error creating libsqld connector", "err", err)
+			return nil, err
+		}
+		slog.Debug("libsql connector created")
+		defer connector.Close()
 
-	connector, err := libsql.NewEmbeddedReplicaConnector(config.Path, config.Remote)
-	if err != nil {
-		slog.Error("error creating libsql connector", "err", err)
-		return nil, errors.New("test")
+		sqlDb = sql.OpenDB(connector)
+		// defer sqlDb.Close() // TODO
+
+		if err := connector.Sync(); err != nil {
+			slog.Error("error syncing to libsqld", "err", err)
+			return nil, err
+		}
+	} else {
+		slog.Debug("using local only")
+		sqlDb, err = sql.Open("libsql", "file:"+config.Path)
+		if err != nil {
+			slog.Error("error opening database", "err", err)
+			return nil, err
+		}
+		// defer sqlDb.Close() // TODO
 	}
-	slog.Debug("libsql connector created")
-	defer connector.Close()
-
-	sqlDb := sql.OpenDB(connector)
-	// defer sqlDb.Close() // TODO
 
 	queries := db.New(sqlDb)
 
@@ -44,11 +59,11 @@ func NewRepository(config *config.DatabaseConfig, migrations *embed.FS) (*Reposi
 	goose.SetBaseFS(*migrations)
 
 	if err := goose.SetDialect("sqlite3"); err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	if err := goose.Up(sqlDb, "migrations"); err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	return &Repository{
--- server/migrations/20240504163622_rename_identity_idp_id.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-alter table identities rename column kratos_id to idp_id;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-alter table identities rename column idp_id to kratos_id;
--- +goose StatementEnd
