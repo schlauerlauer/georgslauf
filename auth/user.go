250e1f4b2b9b3090955c472d0edc9096d605f513
Janek Lauer <git@lauer.email>
2025-02-17 18:29:45 +0100

    update

---
+++ auth/user.go
@@ -0,0 +1,5 @@
+package auth
+
+type UserInfo struct {
+	Name string
+}
--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ COMMIT_HASH="$(git rev-parse --short HEAD)"
 BUILD_TIMESTAMP=$(date '+%Y-%m-%dT%H:%M:%S')
 
 LDFLAGS=(
-  "-X '${PACKAGE}/interfaces.version=${VERSION}'"
-  "-X '${PACKAGE}/interfaces.commitHash=${COMMIT_HASH}'"
-  "-X '${PACKAGE}/interfaces.buildTime=${BUILD_TIMESTAMP}'"
+  "-X '${PACKAGE}/handler.version=${VERSION}'"
+  "-X '${PACKAGE}/handler.commitHash=${COMMIT_HASH}'"
+  "-X '${PACKAGE}/handler.buildTime=${BUILD_TIMESTAMP}'"
 )
 
-CGO_ENABLED=0 go build -ldflags="${LDFLAGS[*]}" -o ./tmp/main .
+CGO_ENABLED=0 go build -ldflags="${LDFLAGS[*]}" -o ./tmp/georgslauf .
