84c18864a62dde9a5cd218e4563228363c3e9b2e
Janek Lauer <git@lauer.email>
2025-03-31 18:50:22 +0200

    change themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,8 +22,8 @@ module.exports = {
 		require('@tailwindcss/typography'),
 	],
 	daisyui: {
-		themes: ["pastel", "cupcake", "dracula", "sunset"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dracula", // name of one of the included themes for dark mode
+		themes: ["nord", "night"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+		darkTheme: "night", // name of one of the included themes for dark mode
 		base: true, // applies background color and foreground color for root element by default
 		styled: true, // include daisyUI colors and design decisions for all components
 		utils: true, // adds responsive and modifier utility classes
